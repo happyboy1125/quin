18aa5073ccfda5f7080b8b83135d7cb6b1c506ec
happyboy1125 <89345974+happyboy1125@users.noreply.github.com>
2022-07-09 07:53:34 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'add58do.herokuapp.com'
+      url.hostname = 'peo68ge.herokuapp.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
